3798127364e84af293c4d50beaf7b1b50b81277c
Jad <jadmail01@gmail.com>
2022-05-27 09:53:18 -0400

    Quiz questions skeleton format

---
+++ Activity7/script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Author : Jad H
+
+TOTAL=0
+
+# Only allow Y N as choice
+echo "Easy - Is Jad amazing:"
+while [ true ]
+do
+    read a
+    if [[ "$a" =~ [Yy]+$ ]] && [[ ${#a} == 1 ]]
+    then
+        TOTAL=$(($TOTAL+1))
+        break
+    elif [[ "$a" =~ [Nn]+$ ]] && [[ ${#a} == 1 ]]
+    then
+        # TOTAL=$(($TOTAL+1))
+        break
+    fi
+    echo "Re-enter answer (Y / N):"
+done
+
+echo $TOTAL
